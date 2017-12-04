96bea5d804d85b8b963527d129e89ef3d12ec564
ShangTseTsai <seantsai67@gmail.com>
2017-12-05 03:03:22 +0800

    Added webdriver
    - Selenium can open browser now.
    - cannot click “next page”

--- crawler.py
+++ crawler.py
@@ -1,33 +1,30 @@
-# import
+#import
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import time
 from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
-import urllib
+import random
+import time
 
-# set destination
-url = 'https://rent.591.com.tw/home/rent/index/r1s7k3.html?kind=1&region=1&section=7'
+# open browser(Firefox)
+browser = webdriver.Firefox()
+# get to the target page
+browser.get('https://rent.591.com.tw/home/rent/index/r1s7k3.html?kind=1&region=1&section=7')
+# start crawler
+soup = BeautifulSoup(browser.page_source, "lxml")
+while len(soup.select('.pageNext')) > 0:
+#    for price in soup.select('.price i'):
+#        print(price.text)
 
-# request
-req = urllib.request.Request(url,data=None,headers={'User-Agent': 'Mozilla/5.0'})
+#pending for resolution
+#    for name in soup.select('h3 a'):
+#        print(name.text)
+    for address in soup.select('p.lightBox em'):
+        print(address.text)
 
-# now return the html
-html = urllib.request.urlopen(req)
-
-# find all links of the results, these are the rentals we are looking for
-sp = BeautifulSoup(html, 'html.parser')
-
-# name
-for name in sp.select('h3 a'):
-    print(name.text)
-
-# price
-for price in sp.select('.price i'):
-    print(price.text)
-
-# address
-for address in sp.select('p.lightBox em'):
-    print(address.text)
-
-# size
-#for size in sp.select('p.lightBox #text'):
-#    print(size.text)
+    time.sleep(random.randrange(1,5))
+    browser.find_element_by_link_text(u"下一頁").click()
+    soup = BeautifulSoup(browser.page_source, "lxml")
+# close the browser
+browser.close()
