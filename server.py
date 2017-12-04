b6c6dfd812380d567dc84d9a0707411d11e9cb5a
hermuba <b101102109@tmu.edu.tw>
2017-12-04 16:04:32 +0000

    add missing parenthesis

--- server.py
+++ server.py
@@ -54,7 +54,7 @@ def send_message(token, recipient, text):
         }),
         headers={'Content-type': 'application/json'})
     if r.status_code != requests.codes.ok:
-        print r.text
+        print(r.text)
 
 if __name__ == '__main__':
     app.run(debug=True)
