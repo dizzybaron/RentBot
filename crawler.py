a0eb858555d310ccac023f7ab0e036fa2d7cf431
ShangTseTsai <seantsai67@gmail.com>
2017-12-06 02:20:22 +0800

    combine change page and crawler
    - cannot close the browser, pending for resolution.

--- crawler.py
+++ crawler.py
@@ -12,15 +12,16 @@ browser = webdriver.Firefox()
 # get to the target page
 browser.get('https://rent.591.com.tw/home/rent/index/r1s7k3.html?kind=1&region=1&section=7')
 time.sleep(1)
+# close the advertisement window
 browser.find_element_by_id("area-box-close").click()
-browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-
 # start crawler
 soup = BeautifulSoup(browser.page_source, "lxml")
-for address in soup.select('p.lightBox em'):
-    print(address.text)
-#browser.find_element_by_xpath("//a[5]/span").click()
-browser.find_element_by_class_name('pageNext').click()
 
-time.sleep(10)
+while len(soup.select('.pageNext')) > 0:
+    browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    for address in soup.select('p.lightBox em'):
+        print(address.text)
+    browser.find_element_by_class_name('pageNext').click()
+    time.sleep(random.randrange(5,10))
+    soup = BeautifulSoup(browser.page_source, "lxml")
 browser.quit()
