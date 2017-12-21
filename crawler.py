af714491333999b62fccb6c63fcf9e1766bb6835
hermuba <b101102109@tmu.edu.tw>
2017-12-21 11:19:12 +0000

    optimize crawler by chaning flow and replace df with list

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException, WebDriverException
 from selenium.webdriver.common.keys import Keys
 # import time that will help our scraper wait
 import time
@@ -19,46 +19,36 @@ import pandas as pd
 def crawler(browser):
     
     soup = BeautifulSoup(browser.page_source, "lxml")
-    df = pd.DataFrame(columns = ["title", "address", "price", "url"]) # the dataframe to return all the results to user
+
+    title = []
+    web_url = []
+    address = []
+    price = []
     
     while len(soup.select('.pageNext')) > 0:
         print("loading another pg")
-        page_data = pd.DataFrame(columns = ["title", "address", "price", "url"]) # each page generates a dataframe
-        browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        i = 0
-        for name in soup.select('h3 a'):
-            print(name, name.get('href'))
-            page_data.loc[str(i), 'title']  = name.text # returning title of that house)
-            page_data.loc[str(i), 'url']  = name.get('href') # returning title of that house)
-
-            i +=1
-            
-        i = 0
-        for address in soup.select('p.lightBox em'):
-            page_data.loc[str(i), 'address'] = address.text # returning address
-            i +=1
-        i = 0
-        for price in soup.select('.price i'):
-            page_data.loc[str(i), 'price'] = price.text # returning address 
-            i += 1
-        df = df.append(page_data)
-
         
-        if len(df.index) > 10:
-            return(df)
+        name = soup.select('h3 a')
+        title = title + [x.text for x in name]
+        web_url = web_url+[x.get('href') for x in name]
+        address = address + soup.select('p.lightBox em')
+        price = price + soup.select('.price i')
+        print(len(web_url))
+        if len(web_url) > 10:
+            # if we have enough number of results
+            return(title, web_url, address, price)
             break
-        elif len(soup.select('.pageBar .last')) > 0:
-            break
+        else:
+            try:
+                # clicking next page
+                browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                browser.find_element_by_class_name('pageNext').click()
+                soup = BeautifulSoup(browser.page_source, "lxml")
+            except (NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException):
+                print("last page or only one page")
+           
 
-        # try clicking next page
-        try:
-            browser.find_element_by_class_name('pageNext').click()
-        except (NoSuchElementException, ElementNotInteractableException, StaleElementReferenceException):
-            print("last page or only one page")
-        soup = BeautifulSoup(browser.page_source, "lxml")
-
-    return(df)
-
+    return(title, web_url, address, price)
 # click all params
 def clicking(user_series):
     url = 'https://rent.591.com.tw/?'
@@ -112,37 +102,33 @@ def clicking(user_series):
 
     # open window and link to 591
     print("loading url", url, ' into browser Firefox')
-    browser = webdriver.Firefox()
-    browser.get(url)
-    
-    print('start to click')
+    try:
+        browser = webdriver.Firefox()
+        browser.get(url)
 
-    # close the advertisement window
-    print(browser.find_element_by_id("area-box-close"))
-    browser.find_element_by_id("area-box-close").click()
-    
-    
-    browser.find_element_by_xpath("//div[@id='search-location']/span").click()
-    time.sleep(2)
-    browser.find_element_by_xpath("//div[@id='search-location']/span").click()
+        print('start to click')
 
-    # click city
-    xpath = u"(//a[contains(text(),"+ user_series['city']+u")])[2]"
-    browser.find_element_by_xpath(xpath).click()
+        # close the advertisement window
+        print(browser.find_element_by_id("area-box-close"))
+        browser.find_element_by_id("area-box-close").click()
+        browser.find_element_by_xpath("//div[@id='search-location']/span").click()
+        browser.find_element_by_xpath("//div[@id='search-location']/span").click()
 
-    
-    
-        
-            
+        # click city
+        xpath = u"(//a[contains(text(),"+ user_series['city']+u")])[2]"
+        browser.find_element_by_xpath(xpath).click()
 
-    # click search
-    browser.find_element_by_class_name("search-input-btn").click()
-    print("finish")
-    # start crawler
-    df = crawler(browser)
-    return(df)
-    #close the browser
-    browser.quit()
+        # click search
+        browser.find_element_by_class_name("search-input-btn").click()
+        print("finish clicking")
 
+        # start crawler
+        df = crawler(browser)
+        return(df)
+
+        #close the browser
+        browser.quit()
+    except WebDriverException:
+        browser.quit()
 
 
